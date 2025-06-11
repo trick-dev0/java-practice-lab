d2aa5a6441cc28416930b385e4aefce9ab9dcbc7
trick-dev0 <dev.patrickaguiar@gmail.com>
2025-06-11 10:01:43 -0300

    Department class created 1.0

--- demo-dao-jdbc/src/Main.java
+++ demo-dao-jdbc/src/Main.java
@@ -1,5 +1,9 @@
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
 
+        Department obj = new Department(1, "Eletronics");
+        System.out.println(obj);
     }
 }
