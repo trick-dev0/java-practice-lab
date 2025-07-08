80bc5012f77378b26c3a6ffe6a8d2839acd6714a
trick-dev0 <dev.patrickaguiar@gmail.com>
2025-07-08 20:12:37 -0300

    Correção de erros gerais

---
+++ project-JPA/src/main/java/aplicativo/App.java
@@ -0,0 +1,12 @@
+package aplicativo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("connectJpa");
+        EntityManager em = emf.createEntityManager();
+    }
+}
