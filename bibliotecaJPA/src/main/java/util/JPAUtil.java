c8dd0c7a434d2a86ecdccd1a5d632d74fe51701d
trick-dev0 <dev.patrickaguiar@gmail.com>
2025-07-10 20:01:18 -0300

    Classe para conexão e instancia criada - (JAPUtil)

---
+++ bibliotecaJPA/src/main/java/util/JPAUtil.java
@@ -0,0 +1,16 @@
+package util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+    public static final EntityManagerFactory emf =  Persistence.createEntityManagerFactory("connectJPA");
+
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+    public static void closeFactory(){
+        emf.close();
+    }
+}
